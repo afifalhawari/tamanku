645814c8b8a0071baff8a6413ec841bcb7f9f74c
robeth <robethrahmatullah@gmail.com>
2015-08-06 07:31:12 +0700

    Add map directive

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('tamanku', ['ionic', 'tamanku.controllers', 'tamanku.services'])
+angular.module('tamanku', ['ionic', 'tamanku.controllers', 'tamanku.services', 'tamanku.directives'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -44,8 +44,32 @@ angular.module('tamanku.controllers', [])
   };
 })
 
-.controller('TamanController', function($scope, Tamans) {
+.controller('TamanController', function($scope, $ionicLoading, Tamans) {
   $scope.tamans = Tamans.all();
+
+  $scope.mapCreated = function(map) {
+    $scope.map = map;
+  };
+
+  $scope.centerOnMe = function () {
+    console.log("Centering");
+    if (!$scope.map) {
+      return;
+    }
+
+    $scope.loading = $ionicLoading.show({
+      content: 'Getting current location...',
+      showBackdrop: false
+    });
+
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      console.log('Got pos', pos);
+      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      $scope.loading.hide();
+    }, function (error) {
+      alert('Unable to get location: ' + error.message);
+    });
+  };
 })
 
 .controller('TamanDetailController', function($scope, $stateParams, Tamans) {
